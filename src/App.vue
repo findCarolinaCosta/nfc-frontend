ec86242f02ffafee2a12b7e8ffc267d15ecbee6e
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-05-15 02:10:12 -0300

    feat: add vue template and initial setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-3xl font-bold text-center">Hello world!</h1>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+});
+</script>
+
+<style>
+#app {
+}
+</style>
