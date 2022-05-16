5b12a1876d1977c115d6e572124bf3e59fb2ab92
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-05-16 00:00:56 -0300

    feat: implements sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <h1 class="text-3xl font-bold text-center">Hello world!</h1>
+  <div class="grid grid-flow-col grid-cols-[288px] w-full">
+    <SideBar />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import SideBar from "@/components/SideBar.vue";
 
 export default Vue.extend({
   name: "App",
+  components: {
+    SideBar,
+  },
 });
 </script>
-
-<style>
-#app {
-}
-</style>
